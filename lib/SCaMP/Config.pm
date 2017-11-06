d749ef4a4577d30680801fe29bc014cbbedef9d6
jamesabbott <j.abbott@imperial.ac.uk>
2017-11-06 14:18:37 +0000

    Added autoloaded accessors to SCaMP::Config, and database configuration from 00_build_ref_db

--- lib/SCaMP/Config.pm
+++ lib/SCaMP/Config.pm
@@ -14,11 +14,45 @@ package SCaMP::Config;
 =head1 DESCRIPTION
 
     Provides configuration data from etc/SCaMP.yaml via an OO interface
+    An autoloaded accessor is created for each top-level entyr in the YAML file 
+    i.e. get_databases()
 
 =cut
 
 use Carp qw(croak);
 use YAML::XS qw(LoadFile);
+
+use vars '$AUTOLOAD';                                
+
+{                                                    
+    my %_attrs = (                                   
+                   _database_dir    => ['read'],     
+                   _databases	    => ['read'],     
+                 );                                  
+
+    sub _accessible {                                
+        my ( $self, $attr, $mode ) = @_;             
+        return $_attrs{$attr}[0] =~ /$mode/;         
+    }                                                
+
+}                                                    
+
+sub DESTROY {                                        
+    my $self = shift;                                
+    return ();
+}
+
+sub AUTOLOAD {
+
+    my ( $self, $newval ) = @_;
+
+    $AUTOLOAD =~ /.*::get(_\w+)/
+      and $self->_accessible( $1, 'read' )
+      and return $self->{$1};
+
+    croak "No such method :$AUTOLOAD";
+
+}
 
 sub new {
 
@@ -35,13 +69,11 @@ sub _init {
 
 	my $self = shift;
 	my $config = LoadFile("$FindBin::Bin/../etc/SCaMP.yaml");
+	foreach my $key(keys %$config) {
+	    $self->{"_${key}"}=$config->{$key};
+	}
 
-	$self->{'config'}=$config;
-}
-
-sub get_config {
-	my $self = shift;
-	return($self->{'config'})
+	return();
 }
 
 1;
