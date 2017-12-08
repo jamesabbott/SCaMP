44231f03abae3611b6a7de326b3a471aa806924c
James Abbott <j.abbott@imperial.ac.uk>
2017-12-08 09:39:27 +0000

    Tested and completed setup script

--- bin/setup.sh
+++ bin/setup.sh
@@ -4,7 +4,7 @@
 # 
 # SCaMP setup script
 #
-# Sets up conda environment and installs prerequsitie packages
+# Sets up conda environment and installs prerequisite packages
 #
 ######################################################################
 
@@ -31,52 +31,82 @@ if [ ${ENV_EXISTS} == 1 ]; then
 	    echo 
 	    echo "Removing SCaMP environment..."
 	    echo
-	    $(conda remove --name SCaMP --all)
+	    conda remove -y --name SCaMP --all
 	    if [ $? != 0 ]; then
-		echo
-		echo "An error occured removing the SCaMP environment..."
-		echo
-		exit 1
+			echo
+			echo "An error occured removing the SCaMP environment..."
+			echo
+			exit 1
 	    fi
 	    break
 	else
 	    echo
-	    echo "Leaving existing SCaMP environment and exiting..."
+	    echo "Leaving existing SCaMP environment in place..."
 	    echo 
-	    exit 1
+		KEEPENV=1
+	    break
 	fi
     done 
 fi
 
+if [ -z "${KEEPENV}" ]; then
+	echo
+	echo "Creating SCaMP environment..."
+	echo
+	conda create -y -n SCaMP
+	if [ $? != 0 ]; then
+    	echo
+    	echo "An error occured creating the SCaMP environment..."
+    	echo
+    	exit 1
+	fi
 
-echo
-echo "Creating SCaMP environment..."
-echo
-$(conda create -n SCaMP)
-if [ $? != 0 ]; then
-    echo
-    echo "An error occured removing the SCaMP environment..."
-    echo
-    exit 1
+	echo "Activating environment..."
+	source activate SCaMP
+	if [ $? != 0 ]; then
+		echo
+		echo "An error occured activating the SCaMP environment..."
+		echo
+		exit 1
+	fi
+
+	echo
+	echo "Installing prerequisite packages..."
+	echo
+	conda install -y --file ${DIR}/../etc/conda_packages.txt
+	if [ $? != 0 ]; then
+		echo
+    	echo "An error occured installing the SCaMP prerequisites"
+    	echo
+    	exit 1
+	fi
 fi
 
-echo "Activating environment..."
-$(source activate SCaMP)
-echo
-if [ $? != 0 ]; then
-    echo
-    echo "An error occured removing the SCaMP environment..."
-    echo
-    exit 1
+PATHSET=$(which SCaMP 2>/dev/null|grep -c SCaMP)
+if [ "${PATHSET} eq 0" ]; then
+	echo
+	echo "Would you like the SCaMP bin directory appending to your default path [y/N]?"
+	echo
+	while true; do
+		read -rsn1 key
+		if [ "${key,,}" = "y" ]; then
+			if [ -e ~/.bashrc ]; then
+				echo "#Added by SCaMP setup.sh" >> ~/.bashrc
+				echo "export PATH=\"${DIR}:\$PATH\"" >> ~/.bashrc
+				echo "Path added to ~/.bashrc..."
+				echo "Logout then login again to ensure PATH is correctly set"
+				echo
+				break
+			fi
+		else
+			break
+		fi
+	done
+else 
+	echo
+	echo "Path already correctly set..."
+	echo
 fi
 
+echo "setup complete..."
 echo
-echo "Installing prerequisite packages..."
-echo
-$(conda install -f ${DIR}../etc/conda_packages.txt)
-if [ $? != 0 ]; then
-    echo
-    echo "An error occured removing the SCaMP environment..."
-    echo
-    exit 1
-fi
