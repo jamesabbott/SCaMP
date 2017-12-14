f49913d671b48680bd0a22f5542f6af270d01a09
James Abbott <j.abbott@imperial.ac.uk>
2017-12-14 16:31:57 +0000

    Documentation updates, including adding POD from scripts

--- doc/build.sh
+++ doc/build.sh
@@ -3,7 +3,9 @@
 for file in qc_trim filter_reads; do
 	outlabel=$(echo ${file^^}|sed 's/_/\\_/g')
 	pod2latex -h1level 3 -modify --out stages/${file}.tex ../bin/${file}
- 	cat stages/${file}.tex |  perl -ne "s/label\{(SYNOPSIS|DESCRIPTION|REQUIRED_ARGUMENTS|OPTIONAL_ARGUMENTS)\}/label\{${outlabel}_\$1\}/g; print" > stages/${file}.tex.tmp
+ 	cat stages/${file}.tex |  \
+		perl -ne "s/label\{(SYNOPSIS|DESCRIPTION|REQUIRED_ARGUMENTS|OPTIONAL_ARGUMENTS|SCAMP_ARGUMENTS|INPUT_AND_OUTPUT_FILES)\}/label\{${outlabel}_\$1\}/g; print" \
+		 > stages/${file}.tex.tmp
 	mv stages/${file}.tex.tmp stages/${file}.tex
 done
 
